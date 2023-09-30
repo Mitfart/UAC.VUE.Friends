1e72def3bbcd1c03766ee6ca3f7d48bd2c2530cb
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-09-30 14:44:50 +0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,17 @@ const usersURL = "http://localhost:3000/users";
 const users = ref([])
 
 const newUser = {
-  name: "test"
+  name: "test_0",
+  details: "test_0_details",
+  avatar: "0.jpg",
+  login: "log_0",
+  password: "p_0",
+}
+
+const activeUser = {
+  name: "test_0",
+  details: "test_0_details",
+  avatar: require(`./assets/images/"0.jpg"`),
 }
 
 
@@ -18,7 +28,7 @@ onMounted(async () => {
 })
 
 
-async function addTodo() {
+async function addUser() {
   await axios
       .post(usersURL, newUser)
       .then(res => {
@@ -31,21 +41,81 @@ async function addTodo() {
 
 
 <template>
-  <div style="display: flex; flex-direction: column">
-    <h1>Users:</h1>
-    <div v-for="user in users" :key="user.id">
-      {{ user.name }}
+  <header>
+    <div class="container">
+      <div class="d-flex justify-content-end">
+        <div class="d-flex align-items-center gap-2">
+          <h6 class="m-0">
+            {{ activeUser.name }}
+          </h6>
+          <img class="rounded-circle border"
+               :src="activeUser.avatar"
+               alt=""
+               width="35" height="35">
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div class="container d-flex flex-column py-5">
+    <div class="d-flex flex-column mb-5">
+      <h1>Users:</h1>
+
+      <div class="d-flex gap-2">
+        <input
+            class="form-control"
+            type="text"
+            placeholder="Add name"
+            v-model="newUser.name"
+        />
+        <input
+            class="form-control"
+            type="text"
+            placeholder="Add details"
+            v-model="newUser.details"
+        />
+        <input
+            class="form-control"
+            type="text"
+            placeholder="Add name"
+            v-model="newUser.login"
+        />
+        <input
+            class="form-control"
+            type="text"
+            placeholder="Add details"
+            v-model="newUser.password"
+        />
+        <button class="btn btn-primary px-5" @click="addUser">
+          Add
+        </button>
+      </div>
     </div>
 
-    <input
-        type="text"
-        aria-label="Add Todo"
-        placeholder="Add Todo"
-        v-model="newUser.name"
-        @keyup.enter="addTodo"
-    />
-  </div>
 
+    <div class="row g-3 align-items-stretch">
+      <div v-for="user in users"
+           :key="user.id"
+           class="col col-lg-4 col-xl-3">
+        <div class="border rounded d-flex flex-column align-items-center text-center h-100 pt-5 p-2">
+          <img class="rounded-circle border"
+               src="#"
+               alt=""
+               width="150" height="150">
+          <h2 class="fw-normal">
+            {{ user.name }}
+          </h2>
+          <p class="flex-grow-1">
+            {{ user.details }}
+          </p>
+          <p><a class="btn btn-primary" href="#">
+            Add Friend &raquo;
+          </a></p>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+
+createApp(App)
+    .use(bootstrap)
+    .mount('#app')
